e0cc300886df91abf38afc827c846fc45bea2dfa
louiserondia <louiserondia@gmail.com>
2024-12-01 15:16:23 +0100

    first commit, day1

--- day01/part1.py
+++ day01/part1.py
@@ -1,7 +1,7 @@
-f = list(map(int, open("input.txt", "r").read().split()))
+data = list(map(int, open("input.txt", "r").read().split()))
 
-t1 = sorted(f[::2])
-t2 = sorted(f[1::2])
+t1 = sorted(data[::2])
+t2 = sorted(data[1::2])
 
 s = sum(abs(t1[i] - t2[i]) for i in range(len(t1)))
 
--- day01/part2.py
+++ day01/part2.py
@@ -1,9 +1,9 @@
 from collections import Counter
 
-f = list(map(int, open("input.txt", "r").read().split()))
+data = list(map(int, open("input.txt", "r").read().split()))
 
-t1 = sorted(f[::2])
-t2 = sorted(f[1::2])
+t1 = sorted(data[::2])
+t2 = sorted(data[1::2])
 
 c = Counter(t2)
 
