74e7700c7b35619eccb9f2017e7bb4245eeae596
louiserondia <louiserondia@gmail.com>
2024-12-04 18:13:16 +0100

    day04

--- day04/part1.py
+++ day04/part1.py
@@ -1,21 +1,20 @@
-dirs = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]
-
-def move(c, d):
-    return (c[0] + d[0], c[1] + d[1])
+DIRS = [1j, 1, -1j, -1, 1 + 1j, -1 - 1j, -1 + 1j, 1 - 1j]
 
 def look_around(coord, grid):
     r = 0
-    for d in dirs:
+    for d in DIRS:
         n = coord
-        s = "".join(grid.get((n := move(n, d)), grid[coord]) for _ in range(3))
+        s = "".join(grid.get((n := n + d), 'O') for _ in range(3))
         if s == "MAS":
             r += 1
     return r
 
 with open("input.txt", "r") as f:
     data = f.read().split('\n')
-    grid = {(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
+    grid = {complex(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
 
-    r = sum(look_around((x, y), grid) for (x, y), e in grid.items() if e == "X")
+    r = sum(look_around(z, grid) for z, e in grid.items() if e == "X")
 
     print (r)
+
+
--- day04/part2.py
+++ day04/part2.py
@@ -1,16 +1,12 @@
-dirs = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
-
-def move(c, d):
-    return (c[0] + d[0], c[1] + d[1])
+DIRS = [-1 - 1j, 1 - 1j, 1 + 1j, -1 + 1j]
 
 def look_around(coord, grid):
-    s = "".join(grid.get(move(coord, d), grid[coord]) for d in dirs)
+    s = "".join(grid.get(coord + d, 'O') for d in DIRS)
     return s in {"MMSS", "SMMS", "SSMM", "MSSM"}
 
 with open("input.txt", "r") as f:
     data = f.read().split('\n')
-    grid = {(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
-
-    r = sum(look_around((x, y), grid) for (x, y), e in grid.items() if e == "A")
+    grid = {complex(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
+    r = sum(look_around(z, grid) for z, e in grid.items() if e == "A")
 
     print (r)
