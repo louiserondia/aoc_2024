cddbe6c810d9891be3b560a93283643442d6e26e
Louise Rondia <louiserondia@gmail.com>
2024-12-08 00:14:53 +0100

    day06

--- day06/part1.py
+++ day06/part1.py
@@ -1,19 +1,15 @@
-from collections import defaultdict
-DIRS = [-1j, 1, 1j, -1]
-
 def s(data, guard):
-    d = 0
+    d = -1j
     r = {guard}
-    while guard + DIRS[d] in data:
-        if data[guard + DIRS[d]] == '#': d = (d + 1) % 4
-        else:
-            guard += DIRS[d]
-            r.add(guard)
+    while guard + d in data:
+        if data[guard + d] == '#': d *= 1j
+        guard += d
+        r.add(guard)
     return len(r)
 
 with open("input.txt", 'r') as f:
     data = f.read().split("\n")
     data = {complex(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
-    start = next((i for i in data if data[i]=="^"), None)
+    start = next(i for i in data if data[i]=="^")
 
     print(s(data, start))
--- day06/part2.py
+++ day06/part2.py
@@ -1,30 +1,33 @@
 
-def p(data, guard, path, d):
-    np = path.copy()
+def p(data, guard, wall):
+    d = -1j
+    path = set((guard, d))
     while guard + d in data:
-        if data[guard + d] == '#': d *= 1j
-        else:
-            guard += d
-            if (guard, d) in np:
-                return 1
-            np.add((guard, d))
+        if (guard, d) in path:
+            return 1
+        path.add((guard, d))
+        if data[guard + d] == '#' or guard + d == wall: d *= 1j
+        else: guard += d
     return 0
 
-def s(data, guard):
+def solve(data, guard):
     d = -1j
-    path = set()
-    r = 0
+    res = set()
+    p0 = guard
     while guard + d in data:
-        if data[guard + d] == '#': d *= 1j
+        if data[guard + d] == "#": 
+            d *= 1j
         else:
-            path.add((guard, d))
-            r += p(data, guard, path, d * 1j)
             guard += d
-    return r
+            if (guard not in res) and p(data, p0, guard):
+                res.add(guard)
+    return res
+
 
 with open("input.txt", 'r') as f:
     data = f.read().split("\n")
     data = {complex(x, y): e for y, row in enumerate(data) for x, e in enumerate(row)}
-    start = next((i for i in data if data[i]=="^"), None)
+    start = next(i for i in data if data[i]=="^")
 
-    print(s(data, start))
+    r = solve(data, start)
+    print(r, len(r))
