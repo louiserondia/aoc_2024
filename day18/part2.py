3f41208e2977878f26ae7f0fe91c5137a1b6defe
louiserondia <louiserondia@gmail.com>
2024-12-19 22:27:30 +0100

    day18

--- day18/part2.py
+++ day18/part2.py
@@ -1,26 +1,21 @@
-DIRS = [1j, -1j, 1, -1]
-
-def traverse(data):
-    parents = {}
-    for k, v in data.items():
-        if v == '.' and k not in parents:
-            parents[k] = k
-            for d in DIRS:
-                if k + d in parents:
-                    union(k + d, k, parents)
-    return parents
-
-def add_parent(x, parents):
-    parents[x] = x
-    for d in DIRS:
-        if x + d in parents:
-            union(x, x + d, parents)
-
 def find(x, parents):
     return find(parents[x], parents) if parents[x] != x else x
 
 def union(x, y, parents):
     parents[find(y, parents)] = find(x, parents)
+
+def add_parent(x, parents):
+    parents[x] = x
+    for d in [1j, -1j, 1, -1]:
+        if x + d in parents:
+            union(x, x + d, parents)
+
+def traverse(data):
+    parents = {}
+    for k, v in data.items():
+        if v == '.' and k not in parents:
+            add_parent(k, parents)
+    return parents
 
 with open('input.txt') as f:
     data = f.read().splitlines()
@@ -28,7 +23,7 @@ with open('input.txt') as f:
     data = { complex(x, y): '#' if complex(x, y) in byte else '.' for y in range(71) for x in range(71) }
 
     parents = traverse(data)
-    for b in byte[::-1]:
+    for b in reversed(byte):
         data[b] = '.'
         add_parent(b, parents)
         if find(complex(0, 0), parents) == find(complex(70, 70), parents):
