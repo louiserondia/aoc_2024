c51a9a42289e81fdd2f008476532cbbaae3ef710
Louise Rondia <louiserondia@gmail.com>
2024-12-26 02:43:09 +0100

    day23 Bron-Kerbosch

--- day23/part2.py
+++ day23/part2.py
@@ -1,25 +1,30 @@
 from collections import defaultdict
-from itertools import combinations
-
-def is_clique(nodes, graph):
-    return not any(b not in graph[a] for a in nodes for b in nodes if a != b)
 
 with open('input.txt') as f:
-    data = f.read().splitlines()
-    data = list(map(lambda l: tuple(l.split('-')), data))
+    edges = f.read().splitlines()
+    edges = list(map(lambda l: tuple(l.split('-')), edges))
 
-    cache = defaultdict(list)
-    for (l, r) in data:
-        cache[l] += [r]
-        cache[r] += [l]
+    neighbours = defaultdict(list)
+    for (l, r) in edges:
+        neighbours[l] += [r]
+        neighbours[r] += [l]
 
-    def find_biggest_clique():
-        keys = list(cache.keys())
-        for size in range(len(keys) - 1):
-            comb = combinations(keys, size)
-            for subset in comb:
-                if (is_clique(subset, cache)):
-                    print(sorted(subset))
-                    break
-    
-    find_biggest_clique()
+    vertices = list(neighbours.keys())
+
+    res = []
+    def bk(r, p, x):
+        global res
+        if not bool(p) and not bool(x):
+            if len(r) > len(res):
+                res = r
+            return
+        pivot = (p + x)[0]
+        for v in filter(lambda e: e not in neighbours[pivot], p):
+            next_p = list(filter(lambda n: n in p, neighbours[v]))
+            next_x = list(filter(lambda n: n in x, neighbours[v]))
+            bk(r + [v], next_p, next_x)
+            p.remove(v)
+            x.append(v)
+
+    bk([], vertices, [])
+    print(','.join(sorted(res)))
