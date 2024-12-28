16014089546c6728a0d77733da2511fea6f52d17
louiserondia <louiserondia@gmail.com>
2024-12-28 14:05:16 +0100

    day24 :)

--- day24/part2.py
+++ day24/part2.py
@@ -1,5 +1,4 @@
-import random
-import operator
+import random, operator
 from itertools import combinations
 
 OPS = {'XOR': operator.xor, 'OR': operator.or_, 'AND': operator.and_}
@@ -7,36 +6,31 @@ OPS = {'XOR': operator.xor, 'OR': operator.or_, 'AND': operator.and_}
 def routine(instructions, info, swaps):
     while instructions:
         executed = set()
-        for instru in instructions:
-            out = instru[3]
-            if instru[0] in info and instru[2] in info: 
-                if instru[3] in swaps:
-                    out = swaps[instru[3]]
-                info[out] = OPS[instru[1]](info[instru[0]], info[instru[2]])
-                executed.add(instru)
+        for (x, op, y, z) in instructions:
+            if x in info and y in info: 
+                info[swaps.get(z, z)] = OPS[op](info[x], info[y])
+                executed.add((x, op, y, z))
         if not executed:
             break
         instructions.difference_update(executed)
 
-    info = reversed([str(info[k]) for k in sorted(info) if k.startswith('z')])
-    return ''.join(info)
+    return ''.join(reversed([str(info[k]) for k in sorted(info) if k.startswith('z')]))
 
 def find_no_no():
-    sure = []
-    maybe = []
+    sure, maybe = [], []
     for (x, op, _, z) in instructions:
-        if z.startswith('z') and z != 'z00' and z != 'z45' and op != 'XOR':
+        if z.startswith('z') and z not in ('z00', 'z45') and op != 'XOR':
             sure.append(z)
-        elif (x.startswith('x') or x.startswith('y')) and int(x[1:]) not in (0, 45) and op == 'XOR':
+        elif x.startswith(('x', 'y')) and int(x[1:]) not in (0, 44) and op == 'XOR':
             nexts = [i for i in instructions if i[0] == z or i[2] == z]
             if all(n[1] != 'XOR' for n in nexts):
                 sure.append(z)
                 missing_z = next(i for i in instructions if i[3] == ('z' + x[1:]))
                 maybe.append((missing_z[0], missing_z[2]))
             else:
-                nexts = next(n for n in nexts if n[1] == 'XOR')
-                if not nexts[3].startswith('z'):
-                    maybe.append((z, nexts[3]))
+                next_xor = next(n for n in nexts if n[1] == 'XOR')[3]
+                if not next_xor.startswith('z'):
+                    maybe.append((z, next_xor))
     return sure, maybe
 
 def gen_bit():
@@ -48,8 +42,7 @@ def gen_bit():
 
 
 with open('input.txt') as f:
-    info, instructions = f.read().split('\n\n')
-    info = { line.split(': ')[0]: int(line.split(': ')[1]) for line in info.splitlines() }
+    instructions = f.read().split('\n\n')[1]
     instructions = set(tuple(line.replace('->', '').split()) for line in instructions.splitlines())
     sure, maybe = find_no_no()
     duos = {(s, m[i]) for s in sure for m in maybe for i in (0, 1)}
@@ -60,13 +53,12 @@ with open('input.txt') as f:
 
         wd = {}
         for q in quatuor:
-            wd[q[0]] = q[1]
-            wd[q[1]] = q[0]
-        for i in range(100):
+            wd[q[0]], wd[q[1]] = q[1], q[0]
+        for i in range(50):
             rx, ry, info = gen_bit()
             target = rx + ry
             res = routine(instructions.copy(), info.copy(), wd)
-            if int(res, 2) != int(target):
-                break
+            if int(res, 2) != int(target): break
         else:
             print(','.join(sorted(wires)))
+            break
